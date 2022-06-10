3e99180302936d0f9d3aa13e122bd365a45b3fb6
PrathyushNair <prathyu360@gmail.com>
2022-06-10 16:34:39 +0530

    added hello

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
+      <div>Hello</div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
